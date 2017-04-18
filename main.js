c88ed2a33c4e6c2f96f6fde14c3768e06f81c7d8
Chris Chiang <cchiang1992@gmail.com>
2017-04-18 16:58:10 -0700

    Check function for validating victory

--- main.js
+++ main.js
@@ -17,12 +17,39 @@ for (var i = 0; i < 9; i ++) {
 // a function to render the state of the board
 var render = boardState => {
   return `
-  ${ boardState[0] || '-' }   ${ boardState[1] || '-' }   ${ boardState[2] || '-' }   
-  ${ boardState[3] || '-' }   ${ boardState[4] || '-' }   ${ boardState[5] || '-' }   
-  ${ boardState[6] || '-' }   ${ boardState[7] || '-' }   ${ boardState[8] || '-' }   
+    ${ boardState[0] || '-' }   ${ boardState[1] || '-' }   ${ boardState[2] || '-' }   
+    ${ boardState[3] || '-' }   ${ boardState[4] || '-' }   ${ boardState[5] || '-' }   
+    ${ boardState[6] || '-' }   ${ boardState[7] || '-' }   ${ boardState[8] || '-' }   
   `
 }
 
+// a function to check if someone won
+var check = boardState => {
+  var s = boardState;
+  if ( (s[0] === 'X' && s[1] === 'X' && s[2] === 'X')
+    || (s[3] === 'X' && s[4] === 'X' && s[5] === 'X')
+    || (s[6] === 'X' && s[7] === 'X' && s[8] === 'X')
+    || (s[0] === 'X' && s[3] === 'X' && s[6] === 'X')
+    || (s[1] === 'X' && s[4] === 'X' && s[7] === 'X')
+    || (s[2] === 'X' && s[5] === 'X' && s[8] === 'X')
+    || (s[0] === 'X' && s[4] === 'X' && s[8] === 'X')
+    || (s[2] === 'X' && s[4] === 'X' && s[6] === 'X')
+    ) {
+    return 'X';
+  }
+  if ( (s[0] === 'Y' && s[1] === 'Y' && s[2] === 'Y')
+    || (s[3] === 'Y' && s[4] === 'Y' && s[5] === 'Y')
+    || (s[6] === 'Y' && s[7] === 'Y' && s[8] === 'Y')
+    || (s[0] === 'Y' && s[3] === 'Y' && s[6] === 'Y')
+    || (s[1] === 'Y' && s[4] === 'Y' && s[7] === 'Y')
+    || (s[2] === 'Y' && s[5] === 'Y' && s[8] === 'Y')
+    || (s[0] === 'Y' && s[4] === 'Y' && s[8] === 'Y')
+    || (s[2] === 'Y' && s[4] === 'Y' && s[6] === 'Y')
+    ) {
+    return 'Y';
+  }
+  return false;
+}
 
 
 print('* * * * * * * * * * * * * *');
